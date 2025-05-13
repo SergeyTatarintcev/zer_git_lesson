9480836ab8451b954b36053f89e383dbd9b7866c
Сергей <Serg18-124@yandex.ru>
2025-05-13 11:55:11 +0300

    add new func summ

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
-def are_circle(r):
-    S = 3.14 * (r ** 2)
-    return S
-
-
-def are_square(a):
-    S_s = a ** 2
-    return S_s
-
-def are_rectangle(a, b):
-    S_r = a * b
-    return S_r
-
-def are_triangle(a, b):
-    S_t = (a * b) / 2
-    return S_t
-
-def are_parallelogram(a, b):
-    S_p = a * b
-    return S_p
-
+def summ(a, b):
+    print(a + b)
---
+++ test.py
@@ -0,0 +1,5 @@
+from main import summ
+
+summ(3, 4)
+summ(56,1)
+summ(67, 5)
